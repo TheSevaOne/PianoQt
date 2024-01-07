cdd333c3782d77bde5cb217f50cdbb381a945653
TheSevaOne <sevarus66@gmail.com>
2024-01-07 03:34:16 +0300

    dir change

---
+++ tablemusicmodel.cpp
@@ -0,0 +1,33 @@
+#include "tablemusicmodel.h"
+
+class TableMusicModelData : public QSharedData
+{
+public:
+
+};
+
+TableMusicModel::TableMusicModel() : data(new TableMusicModelData)
+{
+
+
+}
+
+
+
+TableMusicModel::TableMusicModel(const TableMusicModel &rhs) : data(rhs.data)
+{
+
+}
+
+
+TableMusicModel &TableMusicModel::operator=(const TableMusicModel &rhs)
+{
+    if (this != &rhs)
+        data.operator=(rhs.data);
+    return *this;
+}
+
+TableMusicModel::~TableMusicModel()
+{
+
+}
